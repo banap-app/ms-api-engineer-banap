479d0864c35a06a4814ac73f189f0bcf1102fc6d
Asher Novelli <asherndebortoli@gmail.com>
2025-05-05 16:34:41 -0300

    feat: add shared classes for use in another aggregate root

---
+++ src/shared/domain/errors/InvalidUuidError.ts
@@ -0,0 +1,8 @@
+
+
+export class InvalidUuidError extends Error {
+    constructor(message?: string) {
+        super(message ?? "UUID must be valid")
+        this.name = "InvalidUuidError"
+    }
+}
